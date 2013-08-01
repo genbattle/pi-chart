fb803aa64ef2a5b5c6568e0063e39bff2c41a684
Nick Sarten <gen.battle@gmail.com>
2013-08-01 22:59:13 +1200

    Got basic HTTP Response working with a submit page.
    Still having some trouble getting the image displaying due to threading
    issues with OpenVG and Go's light user-space threads. The trick will be
    to de-mux requests from their independent request goroutines to a single
    drawing thread.

--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"github.com/genbattle/openvg"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-var screenWidth, screenHeight int
+var (
+	screenWidth, screenHeight int
+	submitPage                []byte
+	reqChan chan *http.Request
+)
+
+func drawThread(req <-chan *http.Request) {
+	screenWidth, screenHeight := openvg.Init()
+	openvg.Start(screenWidth, screenHeight)
+	var current *http.Request
+	defer openvg.Finish()
+	// Poll endlessly for requests to draw
+	for {
+		log.Println("Drawing image width ", screenWidth, " height ", screenHeight)
+		log.Println("Got request ", current)
+		openvg.Start(screenWidth, screenHeight)                               // Start the picture
+		openvg.BackgroundColor("black")                           // Black background
+		openvg.FillRGB(44, 100, 232, 1)                           // Big blue marble
+		openvg.FillColor("white")                                 // White text
+		// openvg.TextMid(float32(screenWidth / 2), float32(screenHeight / 2), "hello, world", "serif", screenWidth/10) // Greetings
+		openvg.End()
+		current = <-req
+	}
+}
 
 // Displays the POSTed data on the screen
 func handlePOST(w http.ResponseWriter, r *http.Request) {
@@ -14,35 +39,49 @@ func handlePOST(w http.ResponseWriter, r *http.Request) {
 	// TODO: Check MIME type?
 	// Post the data to the screen
 	// TODO: Replace generic hell world code with something useful
-	openvg.Start(width, height)                               // Start the picture
-	openvg.BackgroundColor("black")                           // Black background
-	openvg.FillRGB(44, 100, 232, 1)                           // Big blue marble
-	openvg.Circle(w2, 0, w)                                   // The "world"
-	openvg.FillColor("white")                                 // White text
-	openvg.TextMid(w2, h2, "hello, world", "serif", width/10) // Greetings
-	openvg.End()
+	reqChan <- r
+	handleGET(w, r)
+}
+
+// Check the HTTP request method
+func handleGET(w http.ResponseWriter, r *http.Request) {
+	w.Write(submitPage)
+	log.Println("Wrote Response length ", len(submitPage))
 }
 
 // Check the HTTP request method
 func handle(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case POST:
+	case "POST", "PUT":
 		handlePOST(w, r)
 	default:
-		log.Println("Receieved non-POST ", r.Method, " Request, ignoring")
+		// TODO: Respond with page that allows user to send images or data to the server
+		handleGET(w, r)
 	}
 }
 
 func main() {
 	// TODO: Set up command-line arguments for port, etc.
+	// Load default response page
+	log.Println("Starting up...")
+	file, err := os.Open("submit.html")
+	if err != nil {
+		log.Println("Error while loading default response page")
+		log.Fatal(err)
+	}
+	submitPage, err = ioutil.ReadAll(file)
+	if err != nil {
+		log.Println("Error while reading default response page from disk")
+		log.Fatal(err)
+	}
 	// Set up the OpenVG rendering
-	screenWidth, screenHeight := openvg.Init()
-	openvg.Start(screenWidth, screenHeight)
-	defer openvg.Finish()
+	reqChan := make(chan *http.Request)
+	go drawThread(reqChan)
 	// Create default handler
-	http.HandleFunc("", handle)
+	http.HandleFunc("/", handle)
+	log.Println("Finished init, listening...")
 	// Start HTTP server listening on port 8787
-	err := http.ListenAndServe(":8787", nil)
+	err = http.ListenAndServe(":8787", nil)
 	if err != nil {
 		log.Println("Error returned by HTTP server:")
 		log.Fatal(err)
